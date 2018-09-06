6d39ed42f8e66c8a019d7953639e8dc26b94e949
Dmitry Ledentsov <dmlled@yahoo.com>
2018-09-06 21:47:04 +0200

    use the properties file

--- src/main/java/github/dled/demo/App.java
+++ src/main/java/github/dled/demo/App.java
@@ -12,9 +12,10 @@ import java.util.concurrent.atomic.AtomicLong;
 public class App {
 
     public static void main(String[] args) {
-        final World world = World.startWithDefaults("playground");
+        // final World world = World.startWithDefaults("playground");
+        final World world = World.start("playground");
         try {
-            final long N = 20_000_000;
+            final long N = 100_000_000;
 
             final Greeter greeter = world.actorFor(Definition.has(ConsoleGreeter.class, Definition.NoParameters), Greeter.class);
 
