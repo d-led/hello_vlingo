dd535b65e3bbc4b715f66b527de680b2d00bfd47
Dmitry Ledentsov <dmlled@yahoo.com>
2021-02-08 16:27:54 +0100

    removed dead code

--- src/main/java/github/dled/demo/ConsoleGreeter.java
+++ src/main/java/github/dled/demo/ConsoleGreeter.java
@@ -4,7 +4,6 @@ import io.vlingo.actors.Actor;
 import io.vlingo.common.Completes;
 
 public class ConsoleGreeter extends Actor implements Greeter {
-    final Completes<Integer> completes = Completes.withSuccess(0);
     Stopwatch sw;
     long count = 0;
 
